637099996189ff983c611175cea7c3122d48e0ea
JoePrinzevalli <joeprinzevalli123@gmail.com>
2022-02-28 13:44:06 -0500

    total is updated for fieldset checkboxes

--- interactive_form_v1/js/script.js
+++ interactive_form_v1/js/script.js
@@ -5,24 +5,61 @@ nameFocus();
 // hides 'other' job role section until user selcts other option//
 const jobRole = document.getElementById('title');
 const other = document.getElementById('other-job-role');
-// const value = jobRole.options[jobRole.selectedIndex];
-console.log(jobRole[6].text)
+other.style.display = 'none';
 
-const jobOption = () => {
-   for (let i = 0; i < jobRole.length; i++ )
-      if (jobRole[i].text === 'Other') {
-            other.style.display = 'initial';
+jobRole.addEventListener('change', () => {
+    if (jobRole.value === 'other') {
+           other.style.display = 'initial';
     } else {
-            other.style.display = 'none';
+           other.style.display = 'none';
+        }
+})
+
+// changes display of text in color dropdown, depending on what desgin user selects //
+
+const design = document.getElementById('design');
+const colorDiv = document.getElementById('shirt-colors');
+const colorSelect = document.getElementById('color');
+
+colorDiv.style.display = 'none';
+
+design.addEventListener('change', () => {   //figure out how to fix hidden and selected buttons //
+        if (design.value === 'js puns') {
+            colorDiv.style.display = 'initial';
+            colorSelect.getElementsByTagName('option')[1].setAttribute('hidden', true)
+            colorSelect.getElementsByTagName('option')[2].setAttribute('hidden', true)
+            colorSelect.getElementsByTagName('option')[3].setAttribute('hidden', true)
+            colorSelect.getElementsByTagName('option')[4].setAttribute('selected', true)
+            colorSelect.getElementsByTagName('option')[5].setAttribute('selected', true)
+            colorSelect.getElementsByTagName('option')[6].setAttribute('selected', true)
+            
+        } else if (design.value === 'heart js'){
+            colorDiv.style.display = 'initial';
+            colorSelect.getElementsByTagName('option')[1].setAttribute('selected', true)
+            colorSelect.getElementsByTagName('option')[2].setAttribute('selected', true)
+            colorSelect.getElementsByTagName('option')[3].setAttribute('selected', true)
+            colorSelect.getElementsByTagName('option')[4].setAttribute('hidden', true)
+            colorSelect.getElementsByTagName('option')[5].setAttribute('hidden', true)
+            colorSelect.getElementsByTagName('option')[6].setAttribute('hidden', true)
+        }
+})
+
+
+
+// updates the totalt cost which reflects the checked activities //
+
+const totalElement = document.getElementById('activities-cost');
+const fieldset = document.getElementById('activities');
+// let checkbox = document.querySelectorAll('input[type=checkbox]') 
+let total = 0;
+
+fieldset.addEventListener('change', (e) => {
+    if (e.target.checked) {
+        total = total + +e.target.getAttribute('data-cost');
+        totalElement.innerHTML = `Total: $${total}`;
+    } else if (e.target.checked === false) {
+        total = total - +e.target.getAttribute('data-cost');
+        totalElement.innerHTML = `Total: $${total}`;
     }
-}
-
-
-jobRole.addEventListener('click',jobOption())
-
-
-
-
-
-
+})
 
